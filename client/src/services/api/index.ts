9b3ef76e14659327509e3bbcf4d99208979301a6
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-10-20 13:18:26 +0200

    Orval

---
+++ client/src/services/api/index.ts
@@ -0,0 +1,31 @@
+import Axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+
+import env from "@/env.mjs";
+
+export const AXIOS_INSTANCE = Axios.create({ baseURL: env.NEXT_PUBLIC_API_URL });
+
+export const API = <T>(
+  config: AxiosRequestConfig,
+  options?: AxiosRequestConfig,
+): Promise<AxiosResponse<T>> => {
+  const source = Axios.CancelToken.source();
+
+  const promise = AXIOS_INSTANCE({
+    ...config,
+    ...options,
+    cancelToken: source.token,
+  }).then(({ data }) => data);
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  promise.cancel = () => {
+    source.cancel("Query was cancelled");
+  };
+
+  return promise;
+};
+
+// In some case with react-query and swr you want to be able to override the return error type so you can also do it here like this
+export type ErrorType<Error> = AxiosError<Error>;
+
+export default API;
