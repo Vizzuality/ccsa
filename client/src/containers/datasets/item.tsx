e3bab1cb554e9734be7551cb7e362abec2341da2
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-10-31 13:06:16 +0100

    Improve layer activation

--- client/src/containers/datasets/item.tsx
+++ client/src/containers/datasets/item.tsx
@@ -13,37 +13,39 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { Switch } from "@/components/ui/switch";
 
 const DatasetsItem = ({ id, attributes }: DatasetListResponseDataItem) => {
-  const lysIds = attributes?.layers?.data?.map((l) => l.id);
+  const lysIds = attributes?.layers?.data?.map((l) => l.id as number);
   const [layers, setLayers] = useSyncLayers();
+
+  const handleToogle = () => {
+    const lys = attributes?.layers;
+
+    if (!lys) return;
+
+    setLayers((prev) => {
+      const ids = lys?.data?.map((l) => {
+        return l.id as number;
+      });
+
+      if (ids) {
+        if (prev.some((id) => ids.includes(id))) {
+          return prev.filter((id) => !ids.includes(id));
+        }
+
+        return [...ids, ...prev];
+      }
+
+      return prev;
+    });
+  };
 
   return (
     <div
       key={id}
-      className="flex items-center justify-between space-x-2.5 rounded-[18px] border p-2.5"
+      className="group flex items-center justify-between space-x-2.5 rounded-[18px] border p-2.5 hover:bg-slate-50"
     >
       <div className="flex items-center justify-start space-x-2.5">
-        <Switch
-          defaultChecked={layers?.some((l) => lysIds?.includes(l))}
-          onCheckedChange={(c: boolean) => {
-            const lys = attributes?.layers;
-
-            if (!lys) return;
-
-            setLayers((prev) => {
-              const ids = lys?.data?.map((l) => {
-                return l.id as number;
-              });
-
-              if (c && ids) return [...ids, ...prev];
-              if (!c && ids) {
-                return prev.filter((id) => !ids.includes(id));
-              }
-
-              return prev;
-            });
-          }}
-        />
-        <button>
+        <Switch checked={layers?.some((l) => lysIds?.includes(l))} onCheckedChange={handleToogle} />
+        <button onClick={handleToogle}>
           <h2>{attributes?.name}</h2>
         </button>
       </div>
