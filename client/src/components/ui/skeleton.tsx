b5530f032dff670f7a2805fcf111d6662f3bb7e9
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-11-01 11:42:23 +0100

    Legend

--- client/src/components/ui/skeleton.tsx
+++ client/src/components/ui/skeleton.tsx
@@ -1,15 +1,7 @@
-import { cn } from "@/lib/classnames"
+import { cn } from "@/lib/classnames";
 
-function Skeleton({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
-      {...props}
-    />
-  )
+function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn("animate-pulse rounded-md bg-muted", className)} {...props} />;
 }
 
-export { Skeleton }
+export { Skeleton };
