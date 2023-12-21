7d6189b1b3d61ce659b53bce54d5016f7b6aa985
Biel Stela <biel.stela@gmail.com>
2023-12-21 16:16:50 +0100

    Datasets and projects ingestion (#7)
    * Add project structure and firts shot to parse projects from excel
    
    * updates notebook to for cleaner generation of projects json
    
    * Adds datasets
    
    * data preprocessing
    
    * clean outputs
    
    * Add pyproj validator to pre commit

---
+++ science/notebooks/utils.py
@@ -0,0 +1,15 @@
+import os
+
+import httpx
+
+
+def get_ids(plural_api_id: str) -> dict[str, int]:
+    """Get Strapi IDs for model"""
+    res = httpx.get(
+        f"https://staging.ccsa.dev-vizzuality.com/cms/api/{plural_api_id}",
+        params={"pagination[pageSize]": 1000},
+        headers={"Authorization": f"bearer {os.getenv('STRAPI_TOKEN')}"},
+    )
+    res.raise_for_status()
+    ids = {e["attributes"]["name"]: e["id"] for e in res.json()["data"]}
+    return ids
