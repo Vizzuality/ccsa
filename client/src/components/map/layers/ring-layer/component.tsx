d73f79d1f48d65f3b2b6bd3d390c1913eff90c80
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-01-08 11:14:05 +0100

    Ring layer: random and style fixes

--- client/src/components/map/layers/ring-layer/component.tsx
+++ client/src/components/map/layers/ring-layer/component.tsx
@@ -2,67 +2,63 @@ import { useMemo } from "react";
 
 import { ScatterplotLayer } from "@deck.gl/layers/typed";
 import { Layer } from "deck.gl/typed";
-import { Feature, FeatureCollection } from "geojson";
 import { RasterSource } from "mapbox-gl";
 
 import { useGetCategories } from "@/types/generated/category";
 
+import GEOJSON from "@/components/map/layers/ring-layer/capitals.json";
 import RingExtension from "@/components/map/layers/ring-layer/extension";
 import { useDeckMapboxOverlay } from "@/components/map/provider";
 
-const GEOSJON = {
-  type: "FeatureCollection",
-  features: [
-    {
-      type: "Feature",
-      properties: {
-        name: "Tokyo",
-        country: "Japan",
-        random: Math.random(),
-      },
-      geometry: {
-        type: "Point",
-        coordinates: [139.6917, 35.6895],
-      },
-    },
-    {
-      type: "Feature",
-      properties: {
-        name: "Paris",
-        country: "France",
-        random: Math.random(),
-      },
-      geometry: {
-        type: "Point",
-        coordinates: [2.3522, 48.8566],
-      },
-    },
-    {
-      type: "Feature",
-      properties: {
-        name: "Washington, D.C.",
-        country: "United States",
-        random: Math.random(),
-      },
-      geometry: {
-        type: "Point",
-        coordinates: [-77.0369, 38.8951],
-      },
-    },
-    {
-      type: "Feature",
-      properties: {
-        name: "Cape Town",
-        country: "South Africa",
-        random: Math.random(),
-      },
-      geometry: {
-        type: "Point",
-        coordinates: [18.4233, -33.918861],
-      },
-    },
-  ],
-} satisfies FeatureCollection;
+// const GEOSJON = {
+//   type: "FeatureCollection",
+//   features: [
+//     {
+//       type: "Feature",
+//       properties: {
+//         name: "Tokyo",
+//         country: "Japan",
+//       },
+//       geometry: {
+//         type: "Point",
+//         coordinates: [139.6917, 35.6895],
+//       },
+//     },
+//     {
+//       type: "Feature",
+//       properties: {
+//         name: "Paris",
+//         country: "France",
+//       },
+//       geometry: {
+//         type: "Point",
+//         coordinates: [2.3522, 48.8566],
+//       },
+//     },
+//     {
+//       type: "Feature",
+//       properties: {
+//         name: "Washington, D.C.",
+//         country: "United States",
+//       },
+//       geometry: {
+//         type: "Point",
+//         coordinates: [-77.0369, 38.8951],
+//       },
+//     },
+//     {
+//       type: "Feature",
+//       properties: {
+//         name: "Cape Town",
+//         country: "South Africa",
+//       },
+//       geometry: {
+//         type: "Point",
+//         coordinates: [18.4233, -33.918861],
+//       },
+//     },
+//   ],
+// } satisfies FeatureCollection;
 
 export interface RingLayerComponentProps {
   id: string;
@@ -81,26 +77,30 @@ export default function RingLayerComponent({ id, visibility, opacity }: RingLaye
   const layer1: Layer = useMemo(() => {
     return new ScatterplotLayer({
       id,
-      data: GEOSJON.features,
+      data: GEOJSON.features,
       opacity: opacity ?? 1,
       visible: visibility ?? true,
       getPosition: (d) => d.geometry.coordinates,
-      getFillColor: [255, 0, 0],
-      getRadius: 10,
+      getFillColor: [125, 125, 125],
+      getRadius: 8,
       radiusUnits: "pixels",
+      pickable: true,
+      onClick: (props) => {
+        console.info(props);
+      },
     });
   }, [id, opacity, visibility]);
 
   const layer2: Layer = useMemo(() => {
     return new ScatterplotLayer({
       id,
-      data: GEOSJON.features,
+      data: GEOJSON.features,
       opacity: opacity ?? 1,
       visible: visibility ?? true,
       getPosition: (d) => d.geometry.coordinates,
-      getFillColor: [255, 0, 0],
-      getRandom: (d: Feature) => d.properties!.random * 1000,
-      getRadius: 10,
+      getFillColor: [125, 125, 125],
+      getRandom: () => Math.random(),
+      getRadius: 8,
       radiusUnits: "pixels",
       extensions: [new RingExtension()],
     });
@@ -108,14 +108,14 @@ export default function RingLayerComponent({ id, visibility, opacity }: RingLaye
 
   useDeckMapboxOverlay({
     id,
-    did: "2",
-    layer: layer2,
+    did: "1",
+    layer: layer1,
   });
 
   useDeckMapboxOverlay({
     id,
-    did: "1",
-    layer: layer1,
+    did: "2",
+    layer: layer2,
   });
 
   return null;
--- client/src/components/map/layers/ring-layer/extension.tsx
+++ client/src/components/map/layers/ring-layer/extension.tsx
@@ -22,8 +22,8 @@ export default class RingExtension extends LayerExtension {
           vRandom = aRandom;
         `,
         "vs:DECKGL_FILTER_SIZE": `
-          const float timespan = 1.5;
-          float wave = smoothstep(0.0, 1.0, fract((uTime - (uStartTime + vRandom)) / timespan));
+          float timespan = 1.5 + vRandom;
+          float wave = smoothstep(0.0, 1.0, fract((uTime - (uStartTime + (vRandom * 1000.0))) / timespan));
           // float wave = sin(uTime * 2.0) * 0.5 + 0.5;
           size *= wave * 3.0;`,
         "fs:#decl": `
@@ -33,12 +33,12 @@ export default class RingExtension extends LayerExtension {
         `,
 
         "fs:DECKGL_FILTER_COLOR": `
-          const float timespan = 1.5;
-          float wave = smoothstep(0.0, 1.0, fract((uTime - (uStartTime + vRandom)) / timespan));
+          float timespan = 1.5 + vRandom;
+          float wave = smoothstep(0.0, 1.0, fract((uTime - (uStartTime + (vRandom * 1000.0))) / timespan));
           // float wave = sin(uTime * 2.0) * 0.5 + 0.5;
           float circle = (geometry.uv.x * geometry.uv.x + geometry.uv.y * geometry.uv.y);
-          vec4 color1 = vec4(1.0, 1.0, 1.0, 0.6);
-          vec4 color2 = vec4(1.0, 0.0, 0.0, 0.8);
+          vec4 color1 = vec4(0.5, 0.5, 0.5, 0.6);
+          vec4 color2 = vec4(0.5, 0.5, 0.5, 0.8);
           color = mix(color1, color2, circle + wave);;
           color.a = 1. - wave;
         `,
