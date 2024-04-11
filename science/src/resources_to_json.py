b8fe460d1f7b9a757dab6223996829fb747c6bd7
Biel Stela <biel.stela@gmail.com>
2024-04-11 16:17:10 +0200

    Obtain existing records ids

--- science/src/resources_to_json.py
+++ science/src/resources_to_json.py
@@ -1,9 +1,17 @@
+import logging
+import os
 from enum import Enum
-from functools import lru_cache, partial
+from functools import lru_cache
+from typing import Annotated
 
-import click
+import httpx
 import polars as pl
-from pydantic import BaseModel, HttpUrl, field_validator
+import typer
+from pydantic import BaseModel, Field, HttpUrl, field_validator
+from rich.logging import RichHandler
+
+log = logging.getLogger(__name__)
+log.addHandler(RichHandler())
 
 
 class ValueType(str, Enum):
@@ -13,11 +21,22 @@ class ValueType(str, Enum):
     resource = "resource"
 
 
+class Output(str, Enum):
+    dataset = "dataset"
+    values = "values"
+    resources = "resources"
+
+
 class Resource(BaseModel):
-    title: str
+    id: int | None = None
+    title: str = Field(serialization_alias="link_title")
     description: str
-    link: HttpUrl
-    country_id: str
+    link: HttpUrl = Field(serialization_alias="link_url")
+    country_id: str = Field(exclude=True)
+
+
+class Resources(BaseModel):
+    resource: list[Resource]
 
 
 class Category(BaseModel):
@@ -25,6 +44,7 @@ class Category(BaseModel):
 
 
 class Dataset(BaseModel):
+    id: int | None = None
     name: str
     category: Category | None = None
     description: str
@@ -70,7 +90,20 @@ def country_to_iso3(file: str, country: str) -> str:
     return get_country_to_iso3(file)[country]
 
 
-def get_dataset(file: str) -> Dataset:
+@lru_cache
+def query_strapi_ids(plural_api_id: str, field_name: str) -> dict[str, int]:
+    """Get Strapi IDs for model"""
+    res = httpx.get(
+        f"https://staging.ccsa.dev-vizzuality.com/cms/api/{plural_api_id}",
+        params={"pagination[pageSize]": 1000},
+        headers={"Authorization": f"bearer {os.getenv('STRAPI_TOKEN')}"},
+    )
+    res.raise_for_status()
+    ids = {e["attributes"][field_name]: e["id"] for e in res.json()["data"]}
+    return ids
+
+
+def get_dataset(file: str, use_id: bool = False) -> Dataset:
     df = pl.read_excel(
         file,
         sheet_name="Dataset Info",
@@ -81,10 +114,15 @@ def get_dataset(file: str) -> Dataset:
         "description": df.select(pl.col("value").filter(pl.col("field") == "Description")).item(),
         "value_type": ValueType.resource,
     }
+    if use_id:
+        _id = query_strapi_ids("datasets", "name").get(data["name"])
+        if _id is None:
+            log.error(f"Dataset \"{data['name']}\" not found in strapi")
+        data["id"] = query_strapi_ids("datasets", "name").get(data["name"])
     return Dataset(**data)
 
 
-def get_dataset_value(file: str) -> DatasetValues:
+def get_dataset_value(file: str, use_id: bool = False) -> DatasetValues:
     df = pl.read_excel(
         file,
         sheet_name="Data",
@@ -94,37 +132,60 @@ def get_dataset_value(file: str) -> DatasetValues:
     for row in df.iter_rows(named=True):
         resources = []
         for name, link, description in zip(row["name"], row["link"], row["descrition"]):
-            resources.append(
-                Resource(
-                    title=name,
-                    link=link,
-                    description=description,
-                    country_id=row["ISO3"],
-                )
+            resource = Resource(
+                title=name, link=link, description=description, country_id=row["ISO3"]
             )
+            if use_id:
+                resource.id = query_strapi_ids("resources", "link_title").get(name)
+            resources.append(resource)
         data_values.append(
-            DatasetValue(dataset= get_dataset(file),resources=resources, country_id=row["ISO3"])
+            DatasetValue(
+                dataset=get_dataset(file, use_id), resources=resources, country_id=row["ISO3"]
+            )
         )
     return DatasetValues(dataset_values=data_values)
 
 
-@click.command()
-@click.argument("input", type=click.Path(exists=True))
-@click.option("--output", "-o", type=click.Choice(["dataset", "values", "resource"]))
-def main(input: str, output: str):
+def main(
+    input: Annotated[str, typer.Argument(...)],
+    output: Annotated[Output, typer.Option("--output", "-o")],
+    use_id: Annotated[
+        bool,
+        typer.Option(is_flag=True, help="Use db ids from strapi instead of full model"),
+    ] = False,
+):
     match output:
         case "dataset":
-            data = get_dataset(input)
+            data = get_dataset(input, use_id)
             print(data.model_dump_json(indent=4))
         case "values":
+            data = get_dataset_value(input, use_id)
+            if use_id:
+                print(
+                    data.model_dump_json(
+                        indent=4,
+                        include={
+                            "dataset_values": {
+                                "__all__": {"country_id", "resource", "dataset"},
+                                "resource": {"__all__": {"id"}},
+                                "dataset": {"__all__": {"id"}},
+                            }
+                        },
+                    )
+                )
+            else:
+                print(data.model_dump_json(indent=4))
+        case "resources":
             data = get_dataset_value(input)
-            print(data.model_dump_json(indent=4))
-        case "resource":
-            data = get_resource(input)
-            print(data.model_dump_json(indent=4))
+            resources = []
+            for value in data.dataset_values:
+                for resource in value.resources:
+                    resources.append(resource)
+            data = Resources(resource=resources)
+            print(data.model_dump_json(indent=4, by_alias=True))
         case _:
             raise ValueError(f"Invalid output: {output}")
 
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+    typer.run(main)
