b6243f097d8df0e57708bf4848d170e95d693307
Bárbara Chaves <barbarabarcellos90@gmail.com>
2024-04-22 13:23:54 +0200

    Update project and country entities (#42)

--- cms/src/api/project-type-of-funding/controllers/project-type-of-funding.ts
+++
@@ -1,7 +0,0 @@
-/**
- * project-type-of-funding controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::project-type-of-funding.project-type-of-funding');
--- cms/src/api/project-type-of-funding/routes/project-type-of-funding.ts
+++
@@ -1,7 +0,0 @@
-/**
- * project-type-of-funding router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::project-type-of-funding.project-type-of-funding');
