00da672845b714af07053422e9dd80490730fed8
Biel Stela <biel.stela@gmail.com>
2024-04-23 11:00:42 +0200

    New project parsing notebooks and horrible refactor of resource to json to handle numeric dtypes

--- science/src/resources_to_json.py
+++ science/src/resources_to_json.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import sys
 from enum import Enum
 from functools import lru_cache
 from typing import Annotated, Any
@@ -125,7 +126,11 @@ def query_strapi_ids(plural_api_id: str, field_name: str) -> dict[str, int]:
     return ids
 
 
-def get_dataset(file: str, use_id: bool = False) -> Dataset:
+def get_dataset(
+    file: str,
+    value_type: ValueType,
+    use_id: bool = False,
+) -> Dataset:
     df = pl.read_excel(
         file,
         sheet_name="Dataset Info",
@@ -134,7 +139,7 @@ def get_dataset(file: str, use_id: bool = False) -> Dataset:
     data = {
         "name": df.select(pl.col("value").filter(pl.col("field") == "Name")).item(),
         "description": df.select(pl.col("value").filter(pl.col("field") == "Description")).item(),
-        "value_type": ValueType.resource,
+        "value_type": value_type,
     }
     if use_id:
         _id = query_strapi_ids("datasets", "name").get(data["name"])
@@ -144,32 +149,44 @@ def get_dataset(file: str, use_id: bool = False) -> Dataset:
     return Dataset(**data)
 
 
-def get_dataset_value(file: str, use_id: bool = False) -> DatasetValues:
+def get_dataset_value(file: str, value_type: ValueType, use_id: bool = False) -> DatasetValues:
     df = pl.read_excel(
         file,
         sheet_name="Data",
     )
-    df = df.with_columns(pl.col("name").str.strip_chars())
+    if value_type == ValueType.resource:
+        df = df.with_columns(pl.col("name").str.strip_chars())
     df = df.group_by("ISO3").agg(pl.all())
     data_values = []
     for row in df.iter_rows(named=True):
         resources = []
+        value = None
         for name, link, description in zip(row["name"], row["link"], row["descrition"]):
-            resource = Resource(
-                title=name, link=link, description=description, country_id=row["ISO3"]
-            )
-            if use_id:
-                resource.id = query_strapi_ids("resources", "link_title").get(name)
-            resources.append(resource)
+            if value_type.value == ValueType.resource:
+                resource = Resource(
+                    title=name, link=link, description=description, country_id=row["ISO3"]
+                )
+                if use_id:
+                    resource.id = query_strapi_ids("resources", "link_title").get(name)
+                resources.append(resource)
+            elif value_type == ValueType.number:
+                value = int(row["name"][-1])
+            elif value_type == ValueType.text:
+                value = row["name"][-1]
 
         country = row["ISO3"]
         if use_id:
             country = query_strapi_ids("countries", "iso3")[country]
+
         data_values.append(
             DatasetValue(
-                dataset=get_dataset(file, use_id),
-                resources=Resources(resource=resources),
+                dataset=get_dataset(file, value_type, use_id),
+                resources=Resources(resources=resources)
+                if value_type.value == ValueType.resource
+                else None,
                 country_id=country,
+                value_number=value if value_type == ValueType.number else None,
+                value_text=value if value_type == ValueType.text else None,
             )
         )
     return DatasetValues(dataset_values=data_values)
@@ -183,24 +200,23 @@ def main(
         bool,
         typer.Option(is_flag=True, help="Use db ids from strapi instead of full model"),
     ] = False,
+    value_type: Annotated[ValueType, typer.Option("--value-type", "-v")] = ValueType.resource,
 ):
     match res_type:
         case "dataset":
-            data = get_dataset(input, use_id)
-            with open(output, "w") as f:
-                f.write(data.model_dump_json(context={"use_id": use_id}, indent=4))
+            data = get_dataset(input, value_type, use_id)
+            exclude = {}
         case "values":
-            data = get_dataset_value(input, use_id)
-            with open(output, "w") as f:
-                f.write(data.model_dump_json(context={"use_id": use_id}, indent=4, by_alias=True))
+            data = get_dataset_value(input, value_type, use_id)
+            exclude = {}
         case "resources":
-            data = get_dataset_value(input)
+            all_data = get_dataset_value(input, value_type, use_id)
             resources_unique = []
             seen = set()
             # deduplicate the resources (based on name) to create the strapi entries
             # for Resource. Then DatasetValues can be related with n existing resources
-            # via id. ( the triple loop is because of the nesting in the models )
-            for value in data.dataset_values:
+            # via id. (the triple loop is because of the nesting in the models)
+            for value in all_data.dataset_values:
                 for resources in value.resources:
                     for resource in resources[1]:
                         if resource.title in seen:
@@ -208,18 +224,18 @@ def main(
                         else:
                             resources_unique.append(resource)
                             seen.add(resource.title)
-            res = Resources(resource=resources_unique)
-            with open(output, "w") as f:
-                f.write(
-                    res.model_dump_json(
-                        context={"use_id": use_id},
-                        indent=4,
-                        by_alias=True,
-                        exclude={"resource": {"__all__": {"id"}}},
-                    )
-                )
+            data = Resources(resource=resources_unique)
+            exclude = {"resource": {"__all__": {"id"}}}
         case _:
             raise ValueError(f"Invalid output: {res_type}")
+
+    with open(output, "w") as f:
+        f = sys.stdout if output == "-" else f
+        f.write(
+            data.model_dump_json(
+                context={"use_id": use_id}, indent=4, by_alias=True, exclude=exclude
+            )
+        )
 
 
 if __name__ == "__main__":
