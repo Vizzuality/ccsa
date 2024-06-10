29331d91e84484f17413c94032f84a6b1f296004
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-06-10 09:22:44 +0200

    Embed: vercel url

--- client/src/components/map/controls/embed/content.tsx
+++ client/src/components/map/controls/embed/content.tsx
@@ -13,23 +13,21 @@ const EmbedContent = () => {
   const [copiedUrl, setCopiedUrl] = useState(false);
   const [copiedIframe, setCopiedIframe] = useState(false);
 
+  const URL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` || env.NEXT_PUBLIC_URL;
+
   return (
     <div className="space-y-3">
       <h3 className="font-display text-lg">Map embed</h3>
 
       <div className="space-y-1">
         <h4 className="font-display text-sm">url</h4>
-        <pre className="whitespace-pre-line break-all bg-gray-50 p-2 text-xs">{`${
-          env.NEXT_PUBLIC_URL
-        }/embed?${searchParams.toString()}`}</pre>
+        <pre className="whitespace-pre-line break-all bg-gray-50 p-2 text-xs">{`${URL}/embed?${searchParams.toString()}`}</pre>
 
         <div className="flex justify-end">
           <Button
             size="sm"
             onClick={() => {
-              navigator.clipboard.writeText(
-                `${env.NEXT_PUBLIC_URL}/embed?${searchParams.toString()}`,
-              );
+              navigator.clipboard.writeText(`${URL}/embed?${searchParams.toString()}`);
               setCopiedUrl(true);
               setTimeout(() => setCopiedUrl(false), 3000);
             }}
@@ -41,9 +39,7 @@ const EmbedContent = () => {
 
       <div className="space-y-1">
         <h4 className="font-display text-sm">iframe</h4>
-        <pre className="whitespace-pre-line break-all bg-gray-50 p-2 text-xs">{`<iframe src="${
-          env.NEXT_PUBLIC_URL
-        }/embed?${searchParams.toString()}" width="800px" height="600px" />
+        <pre className="whitespace-pre-line break-all bg-gray-50 p-2 text-xs">{`<iframe src="${URL}/embed?${searchParams.toString()}" width="800px" height="600px" />
       `}</pre>
 
         <div className="flex justify-end">
@@ -51,9 +47,7 @@ const EmbedContent = () => {
             size="sm"
             onClick={() => {
               navigator.clipboard.writeText(
-                `<iframe src={"${
-                  env.NEXT_PUBLIC_URL
-                }/embed?${searchParams.toString()}"} width="800px" height="600px" />`,
+                `<iframe src={"${URL}/embed?${searchParams.toString()}"} width="800px" height="600px" />`,
               );
               setCopiedIframe(true);
               setTimeout(() => setCopiedIframe(false), 3000);
