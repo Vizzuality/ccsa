afcb792410a635f47e6fce83c60733f710b110e0
yulia-bel <yuliya.beliakova@gmail.com>
2024-06-21 17:05:10 +0200

    Add isOwner policy to Suggestions endpoints

--- cms/src/api/collaborator-edit-suggestion/routes/collaborator-edit-suggestion.ts
+++ cms/src/api/collaborator-edit-suggestion/routes/collaborator-edit-suggestion.ts
@@ -2,6 +2,29 @@
  * collaborator-edit-suggestion router
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreRouter('api::collaborator-edit-suggestion.collaborator-edit-suggestion');
+const isOwner = {
+  name: "global::isOwner",
+  config: { contentType: "collaborator-edit-suggestion" },
+};
+
+export default factories.createCoreRouter("api::collaborator-edit-suggestion.collaborator-edit-suggestion", {
+  config: {
+    create: {
+      policies: [isOwner],
+    },
+    update: {
+      policies: [isOwner],
+    },
+    delete: {
+      policies: [isOwner],
+    },
+    find: {
+      policies: [isOwner],
+    },
+    findOne: {
+      policies: [isOwner],
+    },
+  },
+});
