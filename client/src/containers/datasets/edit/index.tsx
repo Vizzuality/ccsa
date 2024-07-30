7b7bba694f4248a62691d4b2fa109331aef5d56e
María <maluenarod@gmail.com>
2024-07-30 09:28:19 +0200

    resources edition fix

--- client/src/containers/datasets/edit/index.tsx
+++ client/src/containers/datasets/edit/index.tsx
@@ -59,6 +59,17 @@ const getDatasetValues = (
         if (type === "boolean") {
           return { ...acc, [`${countryIso}`]: curr?.attributes?.value_boolean ? true : false };
         }
+
+        if (type === "resource") {
+          return {
+            ...acc,
+            [`${countryIso}`]: curr?.attributes?.resources?.data?.map(({ attributes }) => ({
+              description: attributes?.description,
+              link_title: attributes?.link_title,
+              link_url: attributes?.link_url,
+            })),
+          };
+        }
         return acc;
       },
       {} as Data["data"],
