ff6f3c34800093326ded07f13d3121ac40b2685e
María <maluenarod@gmail.com>
2025-12-26 10:57:56 +0100

    custom endpoint whole search

--- client/src/services/projects/index.ts
+++ client/src/services/projects/index.ts
@@ -1,6 +1,29 @@
-import { UseQueryOptions } from "@tanstack/react-query";
+import {
+  QueryFunction,
+  QueryKey,
+  UseQueryOptions,
+  useQuery,
+  UseQueryResult,
+} from "@tanstack/react-query";
 import axios from "axios";
 import { toast } from "react-toastify";
+
+import type {
+  Country,
+  Error,
+  Pillar,
+  ProjectListResponse,
+  ProjectStatus,
+} from "@/types/generated/strapi.schemas";
+import { API } from "../../services/api/index";
+import type { ErrorType } from "../../services/api/index";
+
+type SecondParameter<T extends (...args: any) => any> = T extends (
+  config: any,
+  args: infer P,
+) => any
+  ? P
+  : never;
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -9,6 +32,7 @@ const api = axios.create({
     Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
   },
 });
+
 export const updateOrCreateProject = async (
   data: unknown,
   userToken: string,
@@ -92,3 +116,107 @@ export function uploadProjectsSuggestionCsv(
       return err;
     });
 }
+
+export const searchByName = async (params: {
+  q?: string;
+  pillars?: Pillar[];
+  countries?: Country[] | null;
+  status?: ProjectStatus[] | null;
+  sortField?: "name" | "status";
+  sortOrder?: "asc" | "desc";
+  page?: number;
+  pageSize?: number;
+}) => {
+  const res = await api.request({
+    method: "get",
+    url: "/projects/search-by-name",
+    params: {
+      q: params.q ?? "",
+      pillars: params.pillars?.join(","),
+      countries: params.countries?.filter(Boolean).join(","),
+      status: params.status?.join(","),
+      sortField: params.sortField ?? "name",
+      sortOrder: params.sortOrder ?? "asc",
+      page: params.page ?? 1,
+      pageSize: params.pageSize ?? 200,
+    },
+  });
+  return res.data;
+};
+
+export type SearchProjectsWholeWordParams = {
+  q?: string;
+  page?: number;
+  pageSize?: number;
+
+  pillars?: number[];
+  countries?: string[];
+  status?: number[];
+
+  sortField?: "name" | "status";
+  sortOrder?: "asc" | "desc";
+};
+
+export const searchProjectsWholeWord = (
+  params?: SearchProjectsWholeWordParams,
+  options?: SecondParameter<typeof API>,
+  signal?: AbortSignal,
+) => {
+  return API<ProjectListResponse>(
+    { url: `/projects/search-by-name`, method: "get", params, signal },
+    options,
+  );
+};
+
+export const getSearchProjectsWholeWordQueryKey = (params?: SearchProjectsWholeWordParams) => {
+  return [`/projects/search-by-name`, ...(params ? [params] : [])] as const;
+};
+
+export const getSearchProjectsWholeWordQueryOptions = <
+  TData = Awaited<ReturnType<typeof searchProjectsWholeWord>>,
+  TError = ErrorType<Error>,
+>(
+  params?: SearchProjectsWholeWordParams,
+  options?: {
+    query?: UseQueryOptions<Awaited<ReturnType<typeof searchProjectsWholeWord>>, TError, TData>;
+    request?: SecondParameter<typeof API>;
+  },
+) => {
+  const { query: queryOptions, request: requestOptions } = options ?? {};
+
+  const queryKey = queryOptions?.queryKey ?? getSearchProjectsWholeWordQueryKey(params);
+
+  const queryFn: QueryFunction<Awaited<ReturnType<typeof searchProjectsWholeWord>>> = ({
+    signal,
+  }) => searchProjectsWholeWord(params, requestOptions, signal);
+
+  return { queryKey, queryFn, ...queryOptions } as UseQueryOptions<
+    Awaited<ReturnType<typeof searchProjectsWholeWord>>,
+    TError,
+    TData
+  > & { queryKey: QueryKey };
+};
+
+export type SearchProjectsWholeWordQueryResult = NonNullable<
+  Awaited<ReturnType<typeof searchProjectsWholeWord>>
+>;
+export type SearchProjectsWholeWordQueryError = ErrorType<Error>;
+
+export const useGetProjectsWholeWordSearch = <
+  TData = Awaited<ReturnType<typeof searchProjectsWholeWord>>,
+  TError = ErrorType<Error>,
+>(
+  params?: SearchProjectsWholeWordParams,
+  options?: {
+    query?: UseQueryOptions<Awaited<ReturnType<typeof searchProjectsWholeWord>>, TError, TData>;
+    request?: SecondParameter<typeof API>;
+  },
+): UseQueryResult<TData, TError> & { queryKey: QueryKey } => {
+  const queryOptions = getSearchProjectsWholeWordQueryOptions(params, options);
+
+  const query = useQuery(queryOptions) as UseQueryResult<TData, TError> & { queryKey: QueryKey };
+
+  query.queryKey = queryOptions.queryKey;
+
+  return query;
+};
