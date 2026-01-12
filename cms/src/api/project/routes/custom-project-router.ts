9587136b3ac20fe61a0a692ced052e0befeefabb
María <maluenarod@gmail.com>
2026-01-12 16:09:30 +0100

    custom endpoint controller

--- cms/src/api/project/routes/custom-project-router.ts
+++ cms/src/api/project/routes/custom-project-router.ts
@@ -1,19 +1,29 @@
 module.exports = {
   routes: [
     {
-      method: 'POST',
-      path: '/projects/approve-project-suggestion',
-      handler: 'project.approveProjectSuggestion',
+      method: "POST",
+      path: "/projects/approve-project-suggestion",
+      handler: "project.approveProjectSuggestion",
       config: {
         policies: [],
         middlewares: [],
       },
     },
     {
-      method: 'POST',
-      path: '/projects/import',
-      handler: 'project.importProjects',
+      method: "POST",
+      path: "/projects/import",
+      handler: "project.importProjects",
       config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: "GET",
+      path: "/projects/search-by-name",
+      handler: "project.searchByNameWholeWord",
+      config: {
+        auth: false,
         policies: [],
         middlewares: [],
       },
